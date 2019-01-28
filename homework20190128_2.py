9e4a5b5db017d386a9cad4f69f72617018808c3d
marcinskowron <marcin.tomasz.skowron@gmail.com>
2019-01-28 08:24:39 +0100

    specs added

--- homework20190128_2.py
+++ homework20190128_2.py
@@ -4,3 +4,10 @@ import pandas as pd
 address = 'https://api.spacexdata.com/v3/launches'
 r = requests.get(address)
 df = pd.DataFrame(r.json())
+
+
+#Korzystając z API SpaceX (https://github.com/r-spacex/SpaceX-API) odpowiedz na pytania:
+#ile z wystrzeleń rakiet zakończyło się sukcesem?
+#który ze statków (tych wodnych) SpaceX ma największy odsetek udanych lądowań?
+#kto najczęściej korzysta z usług SpaceX do wyniesienia satelit na orbitę?
+#który z klientów był najbardziej pechowy (satelity nie na orbicie)
